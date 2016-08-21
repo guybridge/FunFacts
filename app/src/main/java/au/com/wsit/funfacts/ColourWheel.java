b14e5fdbbfc24a446fc896239ed0b356d38d5f7d
Guy Bridge <guy@wsit.com.au>
2016-08-21 08:51:18 +0800

    Added FactBook class

---
+++ app/src/main/java/au/com/wsit/funfacts/ColourWheel.java
@@ -0,0 +1,38 @@
+package au.com.wsit.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by guyb on 20/08/16.
+ */
+public class ColourWheel
+{
+
+    public String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    public String getColour()
+    {
+
+        String colour = "";
+        // Randomly select a fact
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        colour = mColors[randomNumber];
+
+        return colour;
+    }
+}
--- app/src/main/java/au/com/wsit/funfacts/FunFactsActivity.java
+++ app/src/main/java/au/com/wsit/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package au.com.wsit.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,6 +16,9 @@ public class FunFactsActivity extends AppCompatActivity
     // Declare our view variables
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private FactBook mFactbook = new FactBook();
+    private ColourWheel mColourWheel = new ColourWheel();
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,33 +30,19 @@ public class FunFactsActivity extends AppCompatActivity
 
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.layout);
+
+
 
         mShowFactButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                String fact = "";
-                // Randomly select a fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
-
-                // Update the screen with our dynamic fact
+                String fact = mFactbook.getFact();
+                String colour = mColourWheel.getColour();
                 mFactTextView.setText(fact);
-
+                mRelativeLayout.setBackgroundColor(Integer.parseInt(colour));
 
             }
         });
